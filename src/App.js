668ca94c9725515b8aa2f27ef187673877cbf794
Tweetsauce <pobusauce12@gmail.com>
2022-01-13 14:07:09 +0400

    Preview Mode and Edit Mode done: now conditionally render

--- src/App.js
+++ src/App.js
@@ -1,12 +1,63 @@
 import './App.css';
 import React, {Component} from 'react';
-import TestForm from './components/TestForm'
+import TestForm from './components/TestForm';
+import RenderCV from './components/DisplayCV';
 
 class App extends Component {
-  render(){
-    return(
-      <TestForm/>
-    )
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      stateComplete: false,
+
+      name: '',
+      email: '',
+      number: '',
+
+      schoolName: '',
+      degree: '',
+      studyDate: '',
+
+      companyName: '',
+      positionTitle: '',
+      mainWork: '',
+      workLength: '',
+    };
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    if (this.state)
+      this.setState({
+        stateComplete: !this.state.stateComplete,
+      });
+  };
+
+  render() {
+    const buttonText = this.state.stateComplete
+      ? 'Edit Text'
+      : ' Toggle Preview';
+
+    return (
+      <div className="App">
+        <button onClick={this.handleSubmit}>{buttonText}</button>
+        {this.state.stateComplete ? (
+          <RenderCV
+            information={this.state}
+            stateComplete={this.state.stateComplete}
+          />
+        ) : (
+          <TestForm state={this.state} handleChange={this.handleChange} />
+        )}
+      </div>
+    );
   }
 }
 
--- src/components/TestForm.js
+++ src/components/TestForm.js
@@ -1,71 +1,38 @@
 import React, {Component} from 'react';
-import RenderCV from './DisplayCV';
 
 class TestForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      stateComplete: false,
-
-      name: '',
-      email: '',
-      number: '',
-
-      schoolName: '',
-      degree: '',
-      studyDate: '',
-
-      companyName: '',
-      positionTitle: '',
-      mainWork: '',
-      workLength: '',
-    };
   }
-  handleChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-
-    if (this.state)
-      this.setState({
-        stateComplete: !this.state.stateComplete,
-      });
-  };
 
 
   render() {
-    const buttonText = this.state.stateComplete ? 'Edit Text' : ' Toggle Preview'
     return (
       <div className="App">
-        <button onClick={this.handleSubmit}>{buttonText}</button>
         <form>
           <h2>Personal Details</h2>
           <input
             type="text"
             placeholder="Name"
             name="name"
-            value={this.state.name}
-            onChange={e => this.handleChange(e)}
+            value={this.props.state.name}
+            onChange={e => this.props.handleChange(e)}
           />
           <br />
           <input
             type="text"
             placeholder="Email"
             name="email"
-            value={this.state.email}
-            onChange={e => this.handleChange(e)}
+            value={this.props.state.email}
+            onChange={e => this.props.handleChange(e)}
           />
           <br />
           <input
             type="text"
             placeholder="Phone Number"
             name="number"
-            value={this.state.number}
-            onChange={e => this.handleChange(e)}
+            value={this.props.state.number}
+            onChange={e => this.props.handleChange(e)}
           />
           <br></br>
 
@@ -74,63 +41,59 @@ class TestForm extends Component {
             type="text"
             placeholder="School Name"
             name="schoolName"
-            value={this.state.schoolName}
-            onChange={e => this.handleChange(e)}
+            value={this.props.state.schoolName}
+            onChange={e => this.props.handleChange(e)}
           />
           <br />
           <input
             type="text"
             placeholder="Degree"
             name="degree"
-            value={this.state.degree}
-            onChange={e => this.handleChange(e)}
+            value={this.props.state.degree}
+            onChange={e => this.props.handleChange(e)}
           />
           <br />
           <input
             type="text"
             placeholder="Date of Study"
             name="studyDate"
-            value={this.state.studyDate}
-            onChange={e => this.handleChange(e)}
+            value={this.props.state.studyDate}
+            onChange={e => this.props.handleChange(e)}
           />
           <h2>Work Experience</h2>
           <input
             type="text"
             placeholder="Company Name"
             name="companyName"
-            value={this.state.companyName}
-            onChange={e => this.handleChange(e)}
+            value={this.props.state.companyName}
+            onChange={e => this.props.handleChange(e)}
           />
           <br />
           <input
             type="text"
             placeholder="Position Title"
             name="positionTitle"
-            value={this.state.positionTitle}
-            onChange={e => this.handleChange(e)}
+            value={this.props.state.positionTitle}
+            onChange={e => this.props.handleChange(e)}
           />
           <br />
           <input
             type="text"
             placeholder="Main Work"
             name="mainWork"
-            value={this.state.mainWork}
-            onChange={e => this.handleChange(e)}
+            value={this.props.state.mainWork}
+            onChange={e => this.props.handleChange(e)}
           />
           <br />
           <input
             type="text"
             placeholder="Work Length"
             name="workLength"
-            value={this.state.workLength}
-            onChange={e => this.handleChange(e)}
+            value={this.props.state.workLength}
+            onChange={e => this.props.handleChange(e)}
           />
           <br />
         </form>
-        <RenderCV
-          information={this.state}
-          stateComplete={this.state.stateComplete}
-        />
       </div>
     );
   }
